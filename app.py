6ce52b52e9dd0ae8e4aab2134f537b6c5faab20c
Ivan <ivanrj7j@gmail.com>
2020-12-02 19:18:36 +0530

    imported all the modules needed

--- app.py
+++ app.py
@@ -4,3 +4,20 @@ Tkinter for GUI
 For any doubts, you can email me on:
 ivanrj7j@gmail.com
 '''
+
+from tkinter import *
+from tkinter import filedialog
+'''
+the above code will import the tkinter module this
+will be used for making of GUI
+'''
+import os
+'''
+os module will be used for the operating system
+functions
+'''
+import zipfile
+'''
+This is the most important module, as it is going
+to archive and extract the zip file
+'''
