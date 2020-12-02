1ea788e0103cf347e2f7142b130f6780aff8ce3b
Ivan <ivanrj7j@gmail.com>
2020-12-02 19:27:04 +0530

    added the icon and also definded the window

--- app.py
+++ app.py
@@ -21,3 +21,21 @@ import zipfile
 This is the most important module, as it is going
 to archive and extract the zip file
 '''
+
+app_name = 'sZZipper'
+# you can change this as you wish 
+app_icon = 'icon.ico'
+# this is the icon of app, you can change it too 
+
+root = Tk()
+# this is the tkinter object 
+root.geometry('400x200')
+# this defines the dimensions of app 
+root.resizable(False, False)
+# this prevents user from resizing the window 
+root.title(app_name)
+# this makes the title of the app as defined
+root.iconbitmap(app_icon)
+
+root.mainloop()
+# this runs the window 
